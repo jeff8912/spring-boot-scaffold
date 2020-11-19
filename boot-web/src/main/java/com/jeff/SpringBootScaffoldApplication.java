56475aec2b4b6f0ee4100b6c375c8bfbe570d032
jeffloo <jeffloo8912@gmail.com>
2020-11-19 11:26:29 +0800

    mapper-generator

---
+++ boot-web/src/main/java/com/jeff/SpringBootScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.jeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootScaffoldApplication.class, args);
+	}
+
+}
